9fb3cec25ac53f40c4ca9ec8b5b4616fafa77434
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2023-01-29 18:37:21 +0530

    Add input

--- src/components/LangEditor/LangEditor.tsx
+++ src/components/LangEditor/LangEditor.tsx
@@ -1,11 +1,22 @@
 import clsx from 'clsx';
 import React, {FC} from 'react';
 
+import style from './LangEditor.module.scss';
+
 interface LangEditorProps {
   className?: string;
   language?: string;
 }
 
 export const LangEditor: FC<LangEditorProps> = ({className, language}) => {
-  return <div className={clsx('LangEditor', className)}>LangEditor : {language}</div>;
+  return (
+    <div className={clsx(style.LangEditor, className)}>
+      <label htmlFor="langs">Choose a car: {language}</label>
+      <select name="langs" id="langs">
+        <option value="lang1">Hi</option>
+        <option value="lang2">IN</option>
+      </select>
+      <textarea />
+    </div>
+  );
 };
