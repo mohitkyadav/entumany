a10068237220b2843bc8b706cfd0d07b50170474
Mohit Kumar Yadav <mohit.yadav@cobalt.io>
2025-03-21 12:00:18 +0100

    Fix helmet issue

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,14 +1,9 @@
 import React, {FC} from 'react';
-import {Helmet} from 'react-helmet';
 
 interface PageTitleProps {
   title: string;
 }
 
 export const PageTitle: FC<PageTitleProps> = ({title}) => {
-  return (
-    <Helmet>
-      <title>{title}</title>
-    </Helmet>
-  );
+  return <title>{title}</title>;
 };
