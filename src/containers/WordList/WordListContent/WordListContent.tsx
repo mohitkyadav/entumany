b4e371fab197f0a1c89ba29a9c825311fa9b9dcc
Mohit Kumar Yadav <mohit.yadav@cobalt.io>
2025-03-31 17:03:21 +0200

    Add edit mode button

--- src/containers/WordList/WordListContent/WordListContent.tsx
+++ src/containers/WordList/WordListContent/WordListContent.tsx
@@ -8,15 +8,20 @@ import {Pen, Trash2} from 'lucide-react';
 
 export interface WordListContentProps {
   words: WordListItem[][];
+  isEditMode: boolean;
 }
 
-export const WordListContent: FC<WordListContentProps> = ({words}) => {
+export const WordListContent: FC<WordListContentProps> = ({words, isEditMode}) => {
   return (
     <div className={style['word-list-content']}>
       {words.map((saneEntry) => (
         <div key={saneEntry[0].word} className={style['word-list-content__entry']}>
-          <Button leftIcon={<Trash2 size={16} />} onClick={() => alert('delete')} variant="outlined" />
-          <Button leftIcon={<Pen size={16} />} onClick={() => alert('delete')} variant="outlined" />
+          {isEditMode && (
+            <>
+              <Button leftIcon={<Trash2 size={16} />} onClick={() => alert('delete')} variant="outlined" />
+              <Button leftIcon={<Pen size={16} />} onClick={() => alert('delete')} variant="outlined" />
+            </>
+          )}
           <div className={style['word-list-content__entry__word__wrapper']}>
             {saneEntry.map(({id, lang, word}) => (
               <div key={`${id}-${lang}`} className={style['word-list-content__entry__word']}>
