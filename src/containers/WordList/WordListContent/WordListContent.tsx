9877c19eedd8bdba07ec93bccd9e2a3b671966a1
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2025-03-31 17:51:02 +0200

    Add animaitons

--- src/containers/WordList/WordListContent/WordListContent.tsx
+++ src/containers/WordList/WordListContent/WordListContent.tsx
@@ -7,6 +7,7 @@ import {LanguageNames} from 'utils/constants';
 import {EntumanyDB} from 'services/db.service';
 
 import style from './WordListContent.module.scss';
+import clsx from 'clsx';
 
 export interface WordListContentProps {
   words: WordListItem[][];
@@ -29,16 +30,21 @@ export const WordListContent: FC<WordListContentProps> = ({words, isEditMode}) =
       {words.map((saneEntry) => (
         <div key={saneEntry[0].word} className={style['word-list-content__entry']}>
           {isEditMode && (
-            <>
+            <div className={clsx(style['word-list-content__entry__actions'], 'animation-slide-right')}>
               <Button
                 leftIcon={<Trash2 size={16} />}
                 onClick={() => handleDelete(saneEntry[0].id)}
                 variant="outlined"
               />
               <Button leftIcon={<Pen size={16} />} onClick={() => handleEdit(saneEntry[0].id)} variant="outlined" />
-            </>
+            </div>
           )}
-          <div className={style['word-list-content__entry__word__wrapper']}>
+          <div
+            className={clsx(style['word-list-content__entry__word__wrapper'], {
+              'animation-slide-left-wo-op': !isEditMode,
+              'animation-slide-right': isEditMode,
+            })}
+          >
             {saneEntry.map(({id, lang, word}) => (
               <div key={`${id}-${lang}`} className={style['word-list-content__entry__word']}>
                 <span title={LanguageNames[lang]} className={style['word-list-content__entry__word__lang']}>
